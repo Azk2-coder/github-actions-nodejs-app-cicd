c3e104f4c2650f35f75a1123a17610dc568857aa
Tawoong Kang <eidou@naver.com>
2024-12-08 23:07:08 +0900

    update again

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Node JS Servers is  running at http://${hostname}:${port}/`);
+  console.log(`Node JS Servers fix here is  running at http://${hostname}:${port}/`);
 });
 
 module.exports = server;
